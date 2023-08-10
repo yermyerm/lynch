fcfdb155e97d125449ccd9a17e6e94ca9c8de5ce
Yerim Song <141252472+yermyerm@users.noreply.github.com>
2023-08-10 23:32:23 +0900

    JS 추가, 웹폰트 추가, 프로필 수정

---
+++ java.js
@@ -0,0 +1,15 @@
+const hzk = document.querySelector("#hzk img");
+const reo = document.querySelector("#reo img");
+const ysk = document.querySelector("#ysk img");
+const asn = document.querySelector("#asn img");
+const akn = document.querySelector("#akn img");
+
+function dontTouch() {
+  alert("Don't touch");
+}
+
+hzk.addEventListener("click", dontTouch);
+reo.addEventListener("click", dontTouch);
+ysk.addEventListener("click", dontTouch);
+asn.addEventListener("click", dontTouch);
+akn.addEventListener("click", dontTouch);
