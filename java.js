d0aaf7ce368c1b8a3a662c1bc3a5927766bfa547
yermyerm <songkyuu@gmail.com>
2023-08-13 02:17:11 +0900

    JS 수정

--- java.js
+++ java.js
@@ -1,17 +1,8 @@
-const hzk = document.querySelector("#hzk img");
-const reo = document.querySelector("#reo img");
-const ysk = document.querySelector("#ysk img");
-const asn = document.querySelector("#asn img");
-const akn = document.querySelector("#akn img");
-
-const lynch = [hzk, reo, ysk, asn, akn];
-
-function dontTouch() {
-  alert("Don't touch");
+function dontTouch(event) {
+  const lynch = ["HAZUKI", "REO", "YUSUKE", "ASANAO", "AKINORI"];
+  if (lynch.includes(event.srcElement.alt)) {
+    alert("Don't touch!");
+  }
 }
 
-hzk.addEventListener("click", dontTouch);
-reo.addEventListener("click", dontTouch);
-ysk.addEventListener("click", dontTouch);
-asn.addEventListener("click", dontTouch);
-akn.addEventListener("click", dontTouch);
+addEventListener("click", dontTouch);
